07aa108ffb6e2b176d38e85bce05f77b595e3c76
dankelley <kelley.dan@gmail.com>
2020-12-26 11:01:07 -0400

    improve UI

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Ephemeris downloader
+
+This is an R script to download and plot Ephemeris data from a server.  The
+query used for the download is constructed from only a cursory glance at the
+rather extensive documentation (see Resource 1), and so this is really quite
+limited.
 
 # Resources
 1. https://ssp.imcce.fr/webservices/miriade/api/ephemcc/
--- eph.R
+++ eph.R
@@ -2,23 +2,38 @@
 
 library(oce)
 
-query <- "https://ssp.imcce.fr/webservices/miriade/api/ephemcc.php?-name=s:Moon&-type=&-ep=(2020-01-01)&-nbd=5&-step=1d&-tscale=UTC&-observer=@500&-theory=INPOP&-teph=1&-tcoor=1&-oscelem=astorb&-mime=text/csv&-from=MiriadeDoc"
+getEphemeris <- function(name="s:Moon", nbd=5, step=1)
+{
+    query <- paste0("https://ssp.imcce.fr/webservices/miriade/api/ephemcc.php?",
+                    "-name=", name,
+                    "&-type=",
+                    "&-ep=(2020-01-01)",
+                    "&-nbd=", nbd,
+                    "&-step=", step, "d", # FIXME: how to specify in e.g. hours?
+                    "&-tscale=UTC&-observer=@500",
+                    "&-theory=INPOP",
+                    "&-teph=1",
+                    "&-tcoor=1",
+                    "&-oscelem=astorb",
+                    "&-mime=text/csv",
+                    "&-from=MiriadeDoc")
+    eph <- readLines(query)
+    col.names <- c("time", "RA", "DEC", "Dobs", "VMag", "Phase", "Elong.", "dRAcosDEC", "dDEC", "RV")
+    data <- read.csv(text=eph, skip=4, header=FALSE, col.names=col.names)
+    ## RA and DEC are in '[sign]deg min sec' format
+    data$RAdeg <- sapply(data$RA, function(x) {
+                         s <- strsplit(trimws(x), " ")[[1]]
+                         as.integer(s[1]) + as.integer(s[2])/60 + as.integer(s[3])/3600
+                    })
+    data$DECdeg <- sapply(data$DEC, function(x) {
+                          s <- strsplit(trimws(x), " ")[[1]]
+                          as.integer(s[1]) + as.integer(s[2])/60 + as.integer(s[3])/3600
+                    })
+    data$time <- as.POSIXct(data$time, tz="UTC")
+    data
+}
 
-eph <- readLines(query)
-
-col.names <- c("time", "RA", "DEC", "Dobs", "VMag", "Phase", "Elong.", "dRAcosDEC", "dDEC", "RV")
-data <- read.csv(text=eph, skip=4, header=FALSE, col.names=col.names)
-## RA and DEC are in '[sign]deg min sec' format
-data$RAdeg <- sapply(data$RA, function(x) {
-       s <- strsplit(trimws(x), " ")[[1]]
-       as.integer(s[1]) + as.integer(s[2])/60 + as.integer(s[3])/3600
-})
-data$DECdeg <- sapply(data$DEC, function(x) {
-       s <- strsplit(trimws(x), " ")[[1]]
-       as.integer(s[1]) + as.integer(s[2])/60 + as.integer(s[3])/3600
-})
-data$time <- as.POSIXct(data$time, tz="UTC")
-print(data)
+d <- getEphemeris(nbd=28, step=1)
 par(mfrow=c(2, 1))
-oce.plot.ts(data$time, data$RAdeg, type="o", ylab="Right Ascension")
-oce.plot.ts(data$time, data$DECdeg, type="o", ylab="Declination")
+oce.plot.ts(d$time, d$RAdeg, type="o", ylab="Right Ascension")
+oce.plot.ts(d$time, d$DECdeg, type="o", ylab="Declination")
