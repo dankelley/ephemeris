a5ab7c85fd44cabcd9a0392478b248c7e6e03837
dankelley <kelley.dan@gmail.com>
2020-12-28 06:34:44 -0400

    fix sun problem; comment out test (new one coming)

--- R/ephemeris.R
+++ R/ephemeris.R
@@ -7,7 +7,8 @@
 #' and so it requires a web connection to work. It is aimed at people who
 #' already know the ideas and terminology of ephemeris computations.
 #'
-#' Apart from the parameters of this function, the other specifications for
+#' The names of parameters, and their explanaations, are patterned on Reference 1.
+#' Apart from those parameters of this function, the other specifications for
 #' the query are set up in the same way as is used in the query-generation examples.
 #' For example, the 'observer' is set to `@500`, designating the centre of the earth.
 #' Users who are curious about the query should specify `debug=TRUE` when calling
@@ -16,8 +17,9 @@
 #' into a user-adjustable value, via the creation of new parameters
 #' to this function.
 #'
-#' @param name character value of object in question, e.g. `"s:Sun"` for the sun and
-#' `"s:Moon"` for the moon.  Default: `"s:Sun"`.
+#' @param name character value of object in question. There are many objects
+#' that can be checked, e.g. `"p:Sun"` for the sun and
+#' `"s:Moon"` for the moon.  Default: `"p:Sun"`.
 #'
 #' @param longitude decimal value of the longitude. Default: 0.
 #'
@@ -25,36 +27,52 @@
 #'
 #' @param t0 POSIXct time (UTC) for the first retrieval, or a string from which
 #' [as.POSIXct()] can infer such a time. Default: start of present day.
+#' In Reference 1, this is called `ep` and referred to as the requested epoch.
 #'
 #' @param nbd integer value of the number of times to be retrieved. Default: 5.
 #'
 #' @param step numeric value for the time increment, in days.  Default: 1.
 #'
+#' @param theory character value indicating the planetary theory, with choices
+#' `"INPOP"`, `"DE200"`, `"BDL82"`, `"SLP98"`, `"DE403"`,
+#' `"DE405"`, `"DE406"`, `"DE430"`, and `"DE431"`. Default: `"INPOP"`.
+#'
+#' @param teph integer value for the type of ephemeris, with 1
+#' for astrometric J2000, 2 for apparent of the date, 3 for mean
+#' of the date, or 4 for mean J2000. Default: 1.
+#'
+#' @param tcoor integer value for the type of coordinate, with 1
+#' for spherical, 2 for rectangular, 3 for local, 4 for hour angle, and
+#' 5 dedicated to observation. Default: 1.
+#'
+#' @param rplane integer value for the reference plane, with 1
+#' for equator and 2 for ecliptic. Default: 1.
+#'
 #' @param debug logical value indicating whether to print debugging information. At the moment,
 #' the only information printed is the query string. Default: `FALSE`.
 #'
 #' @return `ephemeris` returns a data frame containing columns named
-#' `"time"`, `"RA"` (string hour angle), `"RAdec"` (decimal hour angle),
+#' `"time"`, `"RA"`, `"RAdec"` (decimal hour angle),
 #' `"DEC"` (string hour angle), `"DECdec"` (decimal hour angle),
 #' `"Dobs"`, `"VMag"`, `"Phase"`, `"Elong."`,
-#' `"dRAcosDEC"`, `"dDEC"`, and `"RV"`.  Note that the numeric values
-#' `RAdec` and `DECdec` are computed from the string values `RA` and `DEC` retrieved
-#' from the server.
+#' `"dRAcosDEC"`, `"dDEC"`, and `"RV"`.  In addition, *PROVISIONALLY*
+#' decimal values of `RA` and `DEC` are computed if `tcoor=1`, but
+#' these may be incorrect.
 #'
 #' @examples
 #' # Plot daily Right Ascension and Declination values over a 28-day period.
 #' library(ephemeris)
-#' s <- ephemeris(nbd=28)
+#' s <- ephemeris("p:Sun", nbd=28)
 #' m <- ephemeris("s:Moon", nbd=28)
 #' par(mfrow=c(2, 1), mar=c(3,3,1,2), mgp=c(2,0.7,0))
-#' RAlim <- range(c(s$RAdeg, m$RAdeg))
-#' plot(s$time, s$RAdeg, type="o", xlab="", ylab="RA [hour angle]", col=2, ylim=RAlim)
-#' lines(m$time, m$RAdeg, col=4, type="o")
+#' RAlim <- range(c(s$RAdec, m$RAdec))
+#' plot(s$time, s$RAdec, type="o", xlab="", ylab="Right Ascension [hour]", col=2, ylim=RAlim)
+#' lines(m$time, m$RAdec, col=4, type="o")
 #' mtext("Red: sun", col=2, adj=0)
 #' mtext("Blue: moon", col=4, adj=1)
-#' DEClim <- range(c(s$DECdeg, m$DECdeg))
-#' plot(s$time, s$DECdeg, type="o", xlab="", ylab="DEC [hour angle]", col=2, ylim=DEClim)
-#' lines(m$time, m$DECdeg, col=4, type="o")
+#' DEClim <- range(c(s$DECdec, m$DECdec))
+#' plot(s$time, s$DECdec, type="o", xlab="", ylab="Declination [angle]", col=2, ylim=DEClim)
+#' lines(m$time, m$DECdec, col=4, type="o")
 #'
 #' @author Dan Kelley
 #'
@@ -65,10 +83,11 @@
 #'
 #' @importFrom utils read.csv
 #' @export
-ephemeris <- function(name="s:Sun", longitude=0, latitude=0, t0=Sys.Date(), nbd=5, step=1, debug=FALSE)
+ephemeris <- function(name="p:Sun", longitude=0, latitude=0, t0=Sys.Date(), nbd=5, step=1,
+                     theory="INPOP", teph=1, tcoor=1, rplane=1, debug=FALSE)
 {
     if (is.character(t0))
-        t0 <- as.POSIXct(t0)
+        t0 <- as.POSIXct(t0, tz="UTC")
     t0string <- format(t0, "%Y-%m-%d&nbsp;12h")
     query <- paste0("https://ssp.imcce.fr/webservices/miriade/api/ephemcc.php?",
                     "-name=", name,
@@ -80,31 +99,35 @@ ephemeris <- function(name="s:Sun", longitude=0, latitude=0, t0=Sys.Date(), nbd=
                     "&-step=", step, "d",
                     "&-tscale=UTC",
                     "&-observer=@500",
-                    "&-theory=INPOP",
-                    "&-teph=1",
-                    "&-tcoor=1",
+                    "&-theory=", theory,
+                    "&-teph=", teph,
+                    "&-tcoor=", tcoor,
+                    "&-rplane=", rplane,
                     "&-oscelem=astorb",
                     "&-mime=text/csv",
-                    "&-from=MiriadeDoc")
+                    "&-from=R/ephemeris")
     if (debug)
         cat(query, "\n")
     eph <- readLines(query)
     col.names <- c("time", "RA", "DEC", "Dobs", "VMag", "Phase", "Elong.", "dRAcosDEC", "dDEC", "RV")
     data <- read.csv(text=eph, skip=4, header=FALSE, col.names=col.names)
-    ## RA and DEC are in '[sign]deg min sec' format
-    data$RAdeg <- sapply(data$RA, function(x) {
-                         x <- trimws(x)
-                         sign <- if (grepl("^\\-", x)) -1 else 1
-                         s <- strsplit(x, " ")[[1]]
-                         as.numeric(s[1]) + sign * (as.numeric(s[2])/60 + as.numeric(s[3])/3600)
+    ## RA and DEC are in '[sign]hour min sec' format.
+    ## FIXME(dek 2020-12-27): I think RAdec and DECdec are incorrect.
+    if (tcoor == 1) {
+        data$RAdec <- sapply(data$RA, function(x) {
+                             x <- trimws(x)
+                             sign <- if (grepl("^\\-", x)) -1 else 1
+                             s <- strsplit(x, " ")[[1]]
+                             as.numeric(s[1]) + sign * (as.numeric(s[2])/60 + as.numeric(s[3])/3600)
                     })
-    data$DECdeg <- sapply(data$DEC, function(x) {
-                          x <- trimws(x)
-                          sign <- if (grepl("^\\-", x)) -1 else 1
-                          s <- strsplit(x, " ")[[1]]
-                          as.numeric(s[1]) + sign * (as.numeric(s[2])/60 + as.numeric(s[3])/3600)
+        data$DECdec <- sapply(data$DEC, function(x) {
+                              x <- trimws(x)
+                              sign <- if (grepl("^\\-", x)) -1 else 1
+                              s <- strsplit(x, " ")[[1]]
+                              as.numeric(s[1]) + sign * (as.numeric(s[2])/60 + as.numeric(s[3])/3600)
                     })
-    data$time <- as.POSIXct(data$time, tz="UTC")
+    }
+    data$time <- as.POSIXct(data$time, format="%Y-%m-%dT%H:%M:%S", tz="UTC")
     data
 }
 
